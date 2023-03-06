1b41d9720dabd0164a29cfab721599dfa8471a1a
szy <ziyuan710@163.com>
2023-03-07 00:29:03 +0800

    init

---
+++ src/main/java/org/ziyuan/testgit/TestGitApplication.java
@@ -0,0 +1,17 @@
+package org.ziyuan.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 孙梓源
+ * @date   2023/3/7
+ */
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ziyuan/testgit/TestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ziyuan.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
