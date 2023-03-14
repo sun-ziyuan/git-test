d0ab1d07d40133c3b393b2408383fbcf6ae78515
szy <ziyuan710@163.com>
2023-03-15 07:46:07 +0800

    commit内容测试

--- src/main/java/org/ziyuan/testgit/service/SamethingController.java
+++ src/main/java/org/ziyuan/testgit/service/SamethingController.java
@@ -1,9 +1,16 @@
 package org.ziyuan.testgit.service;
 
+import org.springframework.util.ObjectUtils;
+
 /**
  * @author szy
  * @date 2023/3/7 10:27
  */
 public class SamethingController {
     public static final String TEST = "1";
+
+    public static void main(String[] args) {
+        System.out.println("commit内容写的不好，我要回退代码");
+        System.out.println("commit内容写的太差，我要重新提交");
+    }
 }
